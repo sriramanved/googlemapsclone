df82fc7f3f758d0297373929acf1f47efa508090
Ved Sriraman <sriramanved@gmail.com>
2022-08-12 22:26:35 -0400

    added src files

---
+++ script.js
@@ -0,0 +1,34 @@
+// open with live server!
+
+mapboxgl.accessToken = 'pk.eyJ1IjoidmVkczIyIiwiYSI6ImNsNXJtaXlrdzAwNWkzZG1wOTc1NmprM28ifQ.mNvnY0v1GycahvvSIdCgCA';
+    
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, { enableHighAccuracy: true })
+
+function successLocation(position) {
+    console.log(position)
+    setupMap([position.coords.longitude, position.coords.latitude])
+}
+
+function errorLocation() {
+    setupMap([-2.24, 53.48])
+}
+
+function setupMap(center) {
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: center,
+        zoom: 15
+    })
+
+    const nav = new mapboxgl.NavigationControl();
+    map.addControl(nav);
+
+    var directions = new MapboxDirections({
+        accessToken: mapboxgl.accessToken
+        // unit: 'metric',
+        // profile: 'mapbox/cycling'
+      });
+      
+      map.addControl(directions, 'top-left');
+}
